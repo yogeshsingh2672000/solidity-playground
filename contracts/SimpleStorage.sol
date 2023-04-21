fc3a573c5728c1dc0376c2ae06c24197e7b3055e
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-04-21 18:04:34 +0530

    used struct also

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,21 @@
 pragma solidity ^0.8.8;
 
 contract SimpleStorage {
-    uint32 public hiddenValue = 0;
+    uint256 public hiddenValue = 0;
+    User public userData;
+
+    // kind of an object in contracts
+    struct User {
+        address user;
+        uint256 value;
+    }
 
     event NewUser(address user); // log format
-    event newValue(uint32 value); // log format
+    event newValue(uint256 value); // log format
 
-    function setValue(uint32 _value) public {
+    function setValue(uint256 _value) public {
         hiddenValue = _value;
+        userData = User({user: msg.sender, value: _value});
 
         emit NewUser(msg.sender); // loging
         emit newValue(hiddenValue); //loging
